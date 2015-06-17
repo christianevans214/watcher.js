91ee02034416d0ed105ccb89ea3e83ce3e5707e5
Christian Evans <christian.evans214@gmail.com>
2015-06-16 22:24:36 -0400

    initial commit

--- app.js
+++ app.js
@@ -1,8 +1,55 @@
-console.log(process.argv);
+var fs = require('fs');
+var express = require('express');
+var logger = require('morgan');
+var async = require('async');
+var ejs = require("ejs");
 
+// take a list of files from the command line.
+// now we can watch three files using:
+// node app.js file1.js file2.js file3.js
+//var filenames = Array.prototype.slice.call(process.argv, 2);
+var filenames2 = fs.readdirSync('/Users/christianevans/Documents/CodingProjectsFullStack/IntroToNode/text-files');
+console.log("Node starting...");
+//console.log(filenames);
+//console.log(filenames2.length);
 
+// create the express app
+var app = express();
+app.set('view engine', 'ejs');
 
+// connect the Morgan logging middleware to our app
+app.use( logger('dev') );
 
+// start a server listening on port 1234
+app.listen( 1234 );
 
+//With readFileSync.
+// app.get('/', function (request, response) {
+//   var mapFilenamesToContent = function(file, doneCallBack) {
+//   	var obj = {};
+//    	var compiledFile = fs.readFileSync("./" + file, "utf8");
+//    	obj.id = file.replace(/[^0-9]/gi, "");
+//    	obj.data = compiledFile;
+//    	obj.filename = file;
+//    	doneCallBack(null, obj);
+//   };
 
+//with async readFile.
+app.get('/', function (request, response) {
+  	var mapFilenamesToContent = function(file, doneCallBack) {
+  		var obj = {};
+   		fs.readFile("./" + file,{encoding: "utf8"}, function(err, data){
+   			var obj = {
+   				id: file.replace(/[^0-9]/gi, ""),
+   				data: data,
+   				filename: file
+   			};
+			doneCallBack(null, obj);
+   		});
+  	};
+  	async.map(filenames2, mapFilenamesToContent, function (err, results) {
+    	if (err) console.log('async.map error:', err);
+    	response.render('mainView', {files: results});
+  	});
+});
 
--- sync.js
+++ sync.js
@@ -1,4 +1,14 @@
 console.log("Node starting...");
-// var fs = require('fs');
-// console.log(fs);
-// console.log(fs.readFileSync('./file1.txt'));
+// var greeter = require('./greeter');
+var fs = require('fs');
+//console.log(fs);
+
+//fs.readFileSync will initially return something as a buffer object.
+//Buffer instances are designed to handle raw binary data. Each buffer corresponds to some raw-
+//memory allocated outside V8. Each integer in a buffer is actually represented as a byte.
+//console.log(fs.readFileSync('./file1.txt', 'utf8');
+
+// greeter.shout("CHRISTIAN");
+// greeter.greet("Christian");
+
+
